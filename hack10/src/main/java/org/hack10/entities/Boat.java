b87623d1d3414d7dd8ddaefd2171b3bd684b3ab7
ziadeenyousaf-spec <ziadeenyousaf@outlook.com>
2025-11-29 17:54:46 +0000

    collision maths and shit

--- hack10/src/main/java/org/hack10/entities/Boat.java
+++ hack10/src/main/java/org/hack10/entities/Boat.java
@@ -1,5 +1,8 @@
 package org.hack10.entities;
 
+import javax.imageio.ImageIO;
+import java.awt.Rectangle;
+
 public class Boat extends Entity {
     private Position pos;
 
@@ -7,7 +10,12 @@ public class Boat extends Entity {
     public Boat(Position position)
     {
         pos = position;
-        
+        try{
+            this.image = ImageIO.read(getClass().getResourceAsStream("../../../resources/TopDown.png"));
+            this.hitbox = new Rectangle(position.x, position.y, image.getWidth(), image.getHeight());
+        }catch(Exception e){
+            System.err.println("Error loading boat image: " + e.getMessage());
+        }
     }
 
     //Getters
--- hack10/src/main/java/org/hack10/entities/Tile.java
+++ hack10/src/main/java/org/hack10/entities/Tile.java
@@ -2,7 +2,8 @@ package org.hack10.entities;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.nio.Buffer;
-
+import org.hack10.gamestate.*;
+import java.awt.Rectangle;
 import javax.imageio.ImageIO;
 
 
@@ -10,12 +11,14 @@ public class Tile{
     private BufferedImage image;
     private BufferedImage hitbox;//white and black image for hitbox - white is allowed, black is not - no racial motivations
     private Entity[][] interactables;
+    private Context context;
 
     //constructor
-    public Tile(){
+    public Tile(Context context){
         this.image = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
         this.hitbox = new BufferedImage(32, 32, BufferedImage.TYPE_BYTE_BINARY);
         this.interactables = new Entity[32][32];
+        this.context = context;
     }
 
     //getters
@@ -46,5 +49,25 @@ public class Tile{
     //add interactables to the array
     public void addInteractable(Entity entity, int x, int y){
         this.interactables[x][y] = entity;
+    }
+    public Boolean canBeTravelled(){
+        Position playerCollision = context.getGameState().getBoat().getHitbox();
+        int [] hitboxColours = hitbox.getRGB(playerCollision.x, playerCollision.y, playerCollision.width, playerCollision.height, null, 0, playerCollision.width);
+        for(int colour : hitboxColours){
+            if(colour == -16777216){//black ????????????????????????????
+                return false;
+            }
+        }   
+        for (int i=0;i<interactables.length;i++){//check if boat hitbox hits an interactable hitbox
+            for(int j=0;j<interactables[i].length;j++){
+                Entity entity = interactables[i][j];
+                if(entity != null){
+                    Rectangle entityHitbox = entity.getHitbox();
+                    if(playerCollision.intersects(entityHitbox)){
+                        return false;
+                    }
+                }
+            }
+        }return true;
     }
 }
